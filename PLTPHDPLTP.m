f2dcb8e145f8e3b8dea689fbd36e8e01ea65296b
Zhaohui Zheng（郑朝晖） <46887846+DoctorZheng@users.noreply.github.com>
2026-06-07 15:33:21 +0800

    Update PLTPHDPLTP.m

--- PLTPHDPLTP.m
+++ PLTPHDPLTP.m
@@ -1,72 +1,97 @@
 function  [PLTP_value,HDPLTP_value] = PLTPHDPLTP(I)
 [m,n,h] = size(I);
-if h==3
-    I =  rgb2gray(I);
+if h == 3
+    I = rgb2gray(I);
 end
-I=double(I);
+I = double(I);
 
-PLTP_value = double(zeros([m-4 n-4]));
-HDPLTP_value = double(zeros([m-10-4 n-10-4]));
-PLTP = cell(m-4,n-4);
-HDPLTP = cell(m-10-4,n-10-4);
-thresh1=4;   
+thresh1 = 4;
+thresh0 = 1;
 thresh2 = 1;
 v = mean2(I);
+
+pow3  = 3.^(9:-1:0);
+pow3d = 3.^(7:-1:0);
+
+uMat = conv2(I, ones(5)/25, 'same');
+
+%% ===================== PLTP =====================
+H = m-4;
+W = n-4;
+PLTP_value = zeros(H, W, 'double');
+PLTP_data  = zeros(H*W, 10, 'double');  
+idx = 1;
+
 for i = 3:m-2
+    i2s = i-2:i+2;
     for j = 3:n-2
-        u = mean2(I(i-2:i+2,j-2:j+2)); 
-        out_novertex = (I(i+2,j-2)+I(i+2,j)+I(i+2,j+2)+I(i+1,j-2)+I(i+1,j+2)+I(i-1,j-2)+I(i-1,j+2)+I(i-2,j-2)+I(i-2,j)+I(i-2,j+2))/10.0;
-        in_novertex = (I(i-1,j-1)+I(i-1,j+1)+I(i,j-1)+I(i,j+1)+I(i+1,j))/5.0;
-        out_vertex = (I(i-2,j-1)+I(i-2,j+1)+I(i,j+2)+I(i+2,j+1)+I(i+2,j-1)+I(i,j-2))/6.0;
-        in_vertex = (I(i-1,j)+I(i+1,j+1)+I(i+1,j-1))/3.0;  
-        neighbor01 = ([I(i-2,j-1) I(i-2,j+1) I(i,j+2) I(i+2,j+1) I(i+2,j-1) I(i,j-2) out_vertex out_novertex] - [I(i-1,j) I(i-1,j) I(i+1,j+1) I(i+1,j+1) I(i+1,j-1) I(i+1,j-1) in_vertex in_novertex])>thresh1;
-        neighbor02 = ([I(i-2,j-1) I(i-2,j+1) I(i,j+2) I(i+2,j+1) I(i+2,j-1) I(i,j-2) out_vertex out_novertex] - [I(i-1,j) I(i-1,j) I(i+1,j+1) I(i+1,j+1) I(i+1,j-1) I(i+1,j-1) in_vertex in_novertex])<-thresh1;
-        neighbor11 = ([I(i-1,j) I(i-1,j) I(i+1,j+1) I(i+1,j+1) I(i+1,j-1) I(i+1,j-1) in_vertex in_novertex] - I(i,j))>thresh1;
-        neighbor12 = ([I(i-1,j) I(i-1,j) I(i+1,j+1) I(i+1,j+1) I(i+1,j-1) I(i+1,j-1) in_vertex in_novertex] - I(i,j))<-thresh1;        
-        neighbor0 = neighbor01 - neighbor02;
-        neighbor1 = neighbor11 - neighbor12;        
-        neighbor_u = (neighbor0 + neighbor1) > 0;
-        neighbor_d = (neighbor0 + neighbor1) >= 0;
-        neighbor= neighbor_u + neighbor_d;
+        u = uMat(i,j);
         
-        n00=  [(I(i,j)- v > thresh2) (I(i,j)- u > thresh2)];
-        n01=  [(I(i,j)- v < -thresh2) (I(i,j)- u < -thresh2)];
+        Ii = I(i,j);
+        out_novertex = (I(i+2,j-2)+I(i+2,j)+I(i+2,j+2)+I(i+1,j-2)+I(i+1,j+2)...
+                      + I(i-1,j-2)+I(i-1,j+2)+I(i-2,j-2)+I(i-2,j)+I(i-2,j+2)) * 0.1;
+                  
+        in_novertex  = (I(i-1,j-1)+I(i-1,j+1)+I(i,j-1)+I(i,j+1)+I(i+1,j)) * 0.2;
+        
+        out_vertex   = (I(i-2,j-1)+I(i-2,j+1)+I(i,j+2)+I(i+2,j+1)+I(i+2,j-1)+I(i,j-2))/6;
+        in_vertex    = (I(i-1,j)+I(i+1,j+1)+I(i+1,j-1))/3;
+
+        vec0 = [I(i-2,j-1) I(i-2,j+1) I(i,j+2) I(i+2,j+1) I(i+2,j-1) I(i,j-2) out_vertex out_novertex];
+        vec1 = [I(i-1,j) I(i-1,j) I(i+1,j+1) I(i+1,j+1) I(i+1,j-1) I(i+1,j-1) in_vertex in_novertex];
+        diff0 = vec0 - vec1;
+        diff1 = vec1 - Ii;
+        
+        neighbor01 = diff0 > thresh1;
+        neighbor02 = diff0 < -thresh1;
+        neighbor11 = diff1 > thresh1;
+        neighbor12 = diff1 < -thresh1;
+
+        neighbor0 = neighbor01 - neighbor02;
+        neighbor1 = neighbor11 - neighbor12;
+        temp = neighbor0 + neighbor1;
+        neighbor = (temp > 0) + (temp >= 0);
+
+        n00 = [(Ii-v>thresh0), (Ii-u>thresh0)];
+        n01 = [(Ii-v<-thresh0),(Ii-u<-thresh0)];
         n0 = n00 - n01 + 1;
-        PLTP(i-2,j-2)={[n0 neighbor]};
-        pixel = 0;
-        for k = 1:8
-            pixel = pixel + neighbor(1,k) * 3^(8-k);
-        end
-        pixel = pixel + n0(1) * 3^(8)+ n0(2) * 3^(9);
-        PLTP_value(i-2,j-2) = double(pixel)/230;
+
+  
+        data = [n0, neighbor];
+        PLTP_data(idx,:) = data;
+        idx = idx + 1;
+
+        PLTP_value(i-2,j-2) = (data * pow3') / 230;
     end
 end
 
-m1=m-4;
-n1=n-4;
-thresh3 = 1;
-td = double(zeros([1 8]));
+%% ===================== HDPLTP  =====================
+[m1,n1] = size(PLTP_value);
+Hd = m1-10;
+Wd = n1-10;
+HDPLTP_value = zeros(Hd, Wd, 'double');
+
+pos = [-5 -5; -5 0; -5 5; 0 5; 5 5; 5 0; 5 -5; 0 -5];
+
 for i = 6:m1-5
+    ji = (i-1)*n1;
     for j = 6:n1-5
-        neighbord = [PLTP(i-5,j-5) PLTP(i-5,j) PLTP(i-5,j+5) PLTP(i,j+5) PLTP(i+5,j+5) PLTP(i+5,j) PLTP(i+5,j-5) PLTP(i,j-5)];
+        c = PLTP_data(ji + j, :);
+        td = zeros(1,8);
+        
         for k = 1:8
-            td(k) = sum((cell2mat(PLTP(i,j)) - cell2mat(neighbord(k)))~=0);%pdist([cell2mat(PLTP(i,j));cell2mat(neighbord(k))], 'hamming') * 10;
+            ni = i + pos(k,1);
+            nj = j + pos(k,2);
+            td(k) = sum(c ~= PLTP_data((ni-1)*n1 + nj, :));
         end
-        tdmean=mean(td);
-        tdd=(td-tdmean-thresh3)>0;
-        tdu=(td-tdmean+thresh3)>=0;
-        HDPLTP(i-5,j-5)={tdd+tdu};
-        pixel_d = 0;
-        for k = 1:8
-            pixel_d = pixel_d + (tdd(k)+tdu(k)) * 3^(8-k);
-        end        
-        HDPLTP_value(i-5,j-5) = double(pixel_d)/25;
+        
+        tdmean = mean(td);
+        tdd = td > tdmean + thresh2;
+        tdu = td >= tdmean - thresh2;
+        code = tdd + tdu;
+        
+        HDPLTP_value(i-5,j-5) = sum(code.*pow3d) / 25;
     end
 end
-PLTP_value=int32(PLTP_value); 
-HDPLTP_value=int32(HDPLTP_value); 
-% bins= 256;%8*(8-1) + 3;  
-% result(1,:)=hist(PLTP_value(:),0:(bins-1));%/ numel(lbpIu);
-% result(2,:)=hist(HDPLTP_value(:),0:(bins-1));%/ numel(lbpIu);
-% result(1,:)=result(1,:)/sum(result(1,:));
-% result(2,:)=result(2,:)/sum(result(2,:));
+
+PLTP_value   = int32(PLTP_value);
+HDPLTP_value = int32(HDPLTP_value);
